8c766065fd2412c71d70edc636bf2137800aba6f
aritrikabanerjee <111234834+Aritra901@users.noreply.github.com>
2024-06-10 18:11:59 +0530

    d

---
+++ src/main/java/com/aritra/Practice_/Hibernate/CRUD/Player.java
@@ -0,0 +1,31 @@
+package com.aritra.Practice_.Hibernate.CRUD;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Player {
+	@Id
+	private int player_id;
+	private String player_name;
+	private String team_name;
+	public int getPlayer_id() {
+		return player_id;
+	}
+	public void setPlayer_id(int player_id) {
+		this.player_id = player_id;
+	}
+	public String getPlayer_name() {
+		return player_name;
+	}
+	public void setPlayer_name(String player_name) {
+		this.player_name = player_name;
+	}
+	public String getTeam_name() {
+		return team_name;
+	}
+	public void setTeam_name(String team_name) {
+		this.team_name = team_name;
+	}
+	
+}
